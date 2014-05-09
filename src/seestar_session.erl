45c1e72b90df4250525aef475a85d987269129b2
Patrick Tuckey <patrick.tuckey@rd.io>
2014-05-09 10:38:36 -0700

    Make seestar build with erlang R16B01
    Erlang version 17 requires `dict:dict`,
    but qualifying it this way breaks compilation
    on older versions of erlang.
    
    This change will likely break compilation with
    version 17, but we're using R16B01, so stick
    with that for now.

--- src/seestar_session.erl
+++ src/seestar_session.erl
@@ -52,7 +52,7 @@
          sock :: inet:socket(),
          buffer :: seestar_buffer:buffer(),
          free_ids :: [seestar_frame:stream_id()],
-         backlog = queue:new() :: queue:queue(),
+         backlog = queue:new() :: queue(),
          reqs :: ets:tid()}).
 
 %% -------------------------------------------------------------------------
